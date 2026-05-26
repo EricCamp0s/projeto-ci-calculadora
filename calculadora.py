c7e21db1929641dbdd355b47bdc6df89baf7c7a0
Vanillaiccream <eric.camtex@gmail.com>
2026-05-26 20:39:27 -0300

    fix: Quebrando o build propositalmente para demonstrar falha

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,26 @@
-def somar(a, b):
-    return a + b
+# app.py (alteração proposital para quebrar o teste de soma)
+def soma(a: float, b: float) -> float:
+    """
+    Soma dois números.
+
+    Args:
+        a (float): Primeiro número.
+        b (float): Segundo número.
+
+    Returns:
+        float: A soma de a e b.
+    """
+    return a + b + 1 # ERRO PROPOSITAL AQUI!
+
+def subtrai(a: float, b: float) -> float:
+    """
+    Subtrai dois números.
+
+    Args:
+        a (float): Primeiro número.
+        b (float): Segundo número.
+
+    Returns:
+        float: A subtração de a por b.
+    """
+    return a - b
